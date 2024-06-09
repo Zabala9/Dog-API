7bbb1b22b011284c781dc3ffe674130ac2754217
Steven Zabala <zabalasteven9@gmail.com>
2024-06-09 10:05:38 -0700

    Moving files to folders

--- dog-gallery/src/App.js
+++ dog-gallery/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Gallery from './Components/Gallery';
+import Gallery from './Components/Gallery/Gallery';
+import Main from './Components/MainPage/Main';
 import './App.css';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Gallery />} />
+        <Route exact path='/' element={<Main />} />
+        <Route exact path='/gallery' element={<Gallery />} />
       </Routes>
     </Router>
   );
