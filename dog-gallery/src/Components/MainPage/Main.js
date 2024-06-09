1b90f192a387953494d3aa7a23f9b67dad27e659
Steven Zabala <zabalasteven9@gmail.com>
2024-06-09 10:04:28 -0700

    Creating Main page & passing thru the name to Gallery

---
+++ dog-gallery/src/Components/MainPage/Main.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import './Main.css';
+
+function Main() {
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+
+    const handleChangeName = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleContinue = () => {
+        navigate('/gallery', { state: { name }});
+    };
+
+    return (
+        <div className='main-page'>
+            <label id='label-welcome'>Welcome to Dog's Breed webpage!</label>
+            <p id='description'>
+                In this page you will see pictures from
+                different dog's breed, and also you can select an specific
+                breed you want to see!
+            </p>
+            <label id='label-continue'>To continue, please enter you name bellow</label>
+            <input id='input-name'
+                value={name}
+                onChange={handleChangeName}
+            />
+            <button id='button-continue' onClick={handleContinue}>
+                Continue
+            </button>
+        </div>
+    )
+};
+
+export default Main;
